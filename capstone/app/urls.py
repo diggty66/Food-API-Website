f4b67bce5992d1d1d5bbaa1f422a72b101001df1
SanjayThallam <sanjay.thallam@gmail.com>
2022-11-26 21:34:18 -0500

    Fixed URLS

--- capstone/app/urls.py
+++ capstone/app/urls.py
@@ -2,6 +2,7 @@ from django.urls import re_path, path
 from app import views
 from .API.YelpAPI.yelp import yelp_main, query_api
 from .API.MapsAPI.maps import googlecode
+from .API.SanjayAPI.nutrinix import nutritioncode
 
 # SET THE NAMESPACE!
 app_name = 'app'
@@ -9,6 +10,7 @@ app_name = 'app'
 urlpatterns=[
     path('yelp_main/', yelp_main, name='yelp_main' ),
     path('googlecode/', googlecode, name='googlecode' ),
+    path('nutritioncode/', nutritioncode, name='nutritioncode' ),
     path('query_api/', query_api, name='query_api'),
     path('registration/', views.register_request, name='registration'),
     re_path(r'^user_login/$', views.user_login,name='user_login'),
